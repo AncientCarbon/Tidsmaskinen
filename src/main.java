c751aa1dd7394eb46f196befa70c63db3b388e88
AncientCarbon <boblo.ludvigsen@gmail.com>
2022-04-06 21:55:21 +0200

    main (gen)oprettet

--- src/main.java
+++ src/main.java
@@ -1,2 +1,33 @@
+import java.sql.*;
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) {
+        String host = "localhost";
+        String port = "3306";
+        String database = "tidsmaskinen_db";
+        String cp = "utf8";
+
+        //set username
+        String username = "root";
+        String password = "passwordLmao";
+
+        String url = "jdbc:mysql://" + host + ":" + port + "/" + database + "?characterEncoding=" + cp;
+
+        try{
+            Scanner in = new Scanner(System.in, "CP850");
+            System.out.println("Type sql manipulation: ");
+            String sqlManipulation = in.nextLine();
+            in.close();
+
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sqlManipulation);
+
+            connection.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
